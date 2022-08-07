5acd6289264123b08a0bf429ff7c5bb86a09ebb1
BeliusY <noob.h18.y@gmail.com>
2022-08-07 23:26:16 +0400

    change7

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 databaseConnection();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
